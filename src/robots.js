19481514cf0ce46cf124a1ccf33fb6c567a28485
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-05-02 17:52:49 -0600

    Card components

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Ben Ehlert",
+    username: "bpehlert",
+    email: 'benjamin.ehlert@gmail.com'
+  },
+  {
+    id: 1,
+    name: "Grace Ehlert",
+    username: "ginger92",
+    email: 'grace@dreamlingbooks.com'
+  },
+  {
+      id: 1,
+      name: "Mitchell Stevens",
+      username: "hal88",
+      email: 'mitchell.a.stevens@gmail.com'
+    },
+  {
+    id: 1,
+    name: "Matt Ehlert",
+    username: "@me",
+    email: 'matt@usekeystone.com'
+  },
+]
