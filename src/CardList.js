615d12fbe0144b39cdd0d4d2b367d2c649a70f75
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-05-04 13:25:00 -0600

    No search results found

--- src/CardList.js
+++ src/CardList.js
@@ -2,23 +2,30 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key = {i}
-              id ={robots[i].id}
-              name = {robots[i].name}
-              email = {robots[i].email}
-              username = {robots[i].username}
-            />
-          );
-        })
-      }
-    </div>
-  );
+  if (robots.length > 0) {
+    return (
+      <div>
+        {
+          robots.map((user, i) => {
+            return (
+              <Card
+                key = {i}
+                id ={robots[i].id}
+                name = {robots[i].name}
+                email = {robots[i].email}
+                username = {robots[i].username}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+    return (
+      <div>
+        <p>No Robofriends found.</p>
+      </div>
+    );
 }
 
 export default CardList;
